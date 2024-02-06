3262c047c42ee2b02978ea60e1cd313d87b67d9b
Balaji <balaji0017@gmail.com>
2024-02-06 05:20:15 +0000

    Added Page Object Class

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,13 +3,13 @@ from selenium.webdriver.common.by import By
 
 class LoginPage:
 
-    def __init__(self,driver):
-        self.__driver=driver
+    def __init__(self, driver):
+        self.__driver = driver
 
-    def enter_username(self,username):
+    def enter_username(self, username):
         self.__driver.find_element(By.ID, "authUser").send_keys(username)
 
-    def enter_password(self,password):
+    def enter_password(self, password):
         self.__driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys(password)
 
     def click_on_login(self):
@@ -17,3 +17,19 @@ class LoginPage:
 
     def get_error_message(self):
         return self.__driver.find_element(By.XPATH, "//p[contains(text(),'Invalid')]").text
+
+    @property
+    def get_login_title(self):
+        return self.__driver.title
+
+    @property
+    def get_app_desc(self):
+        return self.__driver.find_element(By.XPATH, "//p[contains(text(),'most')]").text
+
+    @property
+    def get_username_placeholder(self):
+        return self.__driver.find_element(By.ID, "authUser").get_attribute("placeholder")
+
+    @property
+    def get_password_placeholder(self):
+        return self.__driver.find_element(By.CSS_SELECTOR, "#clearPass").get_attribute("placeholder")
--- tests/test_login.py
+++ tests/test_login.py
@@ -13,13 +13,11 @@ class TestLogin(WebDriverWrapper):
 
     @pytest.mark.parametrize("username,password,expected_title", DataSource.data_valid_login)
     def test_valid_login(self, username, password, expected_title):
-
-        login=LoginPage(self.driver)
+        login = LoginPage(self.driver)
         login.enter_username(username)
         login.enter_password(password)
         login.click_on_login()
-
-        main=MainPage(self.driver)
+        main = MainPage(self.driver)
         assert_that(expected_title).is_equal_to(main.get_main_title)
 
     @pytest.mark.parametrize("username,password,expected_error", DataSource.data_invalid_login)
@@ -34,17 +32,15 @@ class TestLogin(WebDriverWrapper):
 
 class TestLoginUI(WebDriverWrapper):
     def test_title(self):
-        print(self.driver)
-        actual_title = self.driver.title
-        assert_that("OpenEMR Login").is_equal_to(actual_title)
+        login = LoginPage(self.driver)
+        assert_that("OpenEMR Login").is_equal_to(login.get_login_title)
 
     def test_app_description(self):
-        actual_desc = self.driver.find_element(By.XPATH, "//p[contains(text(),'most')]").text
-        assert_that(actual_desc).contains("Electronic Health Record and Medical Practice Management")
+        login = LoginPage(self.driver)
+        # actual_desc = login.get_app_desc
+        assert_that(login.get_app_desc).contains("Electronic Health Record and Medical Practice Management")
 
     def test_placeholder(self):
-        actual_username_placeholder = self.driver.find_element(By.ID, "authUser").get_attribute("placeholder")
-        actual_password_placeholder = self.driver.find_element(By.CSS_SELECTOR, "#clearPass").get_attribute(
-            "placeholder")
-        assert_that("Username").is_equal_to(actual_username_placeholder)
-        assert_that("Password").is_equal_to(actual_password_placeholder)
+        login = LoginPage(self.driver)
+        assert_that("Username").is_equal_to(login.get_username_placeholder)
+        assert_that("Password").is_equal_to(login.get_password_placeholder)
