407963c60cdf2b190fa44f712767cb25c7d01d05
Balaji <balaji0017@gmail.com>
2024-02-06 06:44:21 +0000

    Added WebDriverKeyword with locator in init

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,22 +8,28 @@ class LoginPage(WebDriverKeywords):
     def __init__(self, driver):
         super().__init__(driver)
         self.__driver = driver
+        self.__username_locator = (By.ID, "authUser")
+        self.__password_locator = (By.CSS_SELECTOR, "#clearPass")
+        self.__login_locator = (By.ID, "login-button")
+        self.__invalid_error_locator = (By.XPATH, "//p[contains(text(),'Invalid')]")
+        self.__app_desc_locator = (By.XPATH, "//p[contains(text(),'most')]")
 
     def enter_username(self, username):
         # self.__driver.find_element(By.ID, "authUser").send_keys(username)
-        super().type_on_element((By.ID, "authUser"),username)
+        super().type_on_element(self.__username_locator, username)
 
     def enter_password(self, password):
         # self.__driver.find_element(By.CSS_SELECTOR, "#clearPass").send_keys(password)
-        super().type_on_element((By.CSS_SELECTOR, "#clearPass"),password)
+        super().type_on_element(self.__password_locator, password)
 
     def click_on_login(self):
         # self.__driver.find_element(By.ID, "login-button").click()
-        super().click_on_element((By.ID, "login-button"))
+        super().click_on_element(self.__login_locator)
 
     def get_error_message(self):
         # return self.__driver.find_element(By.XPATH, "//p[contains(text(),'Invalid')]").text
-        return super().get_text_from_element((By.XPATH, "//p[contains(text(),'Invalid')]"))
+        return super().get_text_from_element(self.__invalid_error_locator)
+
     @property
     def get_login_title(self):
         return self.__driver.title
@@ -31,14 +37,14 @@ class LoginPage(WebDriverKeywords):
     @property
     def get_app_desc(self):
         # return self.__driver.find_element(By.XPATH, "//p[contains(text(),'most')]").text
-        return super().get_text_from_element((By.XPATH, "//p[contains(text(),'most')]"))
+        return super().get_text_from_element(self.__app_desc_locator)
 
     @property
     def get_username_placeholder(self):
         # return self.__driver.find_element(By.ID, "authUser").get_attribute("placeholder")
-        return super().get_attribute_from_element((By.ID, "authUser"),"placeholder")
+        return super().get_attribute_from_element(self.__username_locator, "placeholder")
 
     @property
     def get_password_placeholder(self):
         # return self.__driver.find_element(By.CSS_SELECTOR, "#clearPass").get_attribute("placeholder")
-        return super().get_attribute_from_element((By.CSS_SELECTOR, "#clearPass"), "placeholder")
+        return super().get_attribute_from_element(self.__password_locator, "placeholder")
--- pages/main_page.py
+++ pages/main_page.py
@@ -7,13 +7,17 @@ class MainPage(WebDriverKeywords):
     def __init__(self, driver):
         super().__init__(driver)
         self.__driver = driver
+        self.__patient_locator=(By.XPATH, "//div[text()='Patient']")
+        self.__new_search_locator=(By.XPATH, "//div[text()='New/Search']")
 
     @property
     def get_main_title(self):
         return self.__driver.title
 
     def click_on_patient_menu(self):
-        self.__driver.find_element(By.XPATH, "//div[text()='Patient']").click()
+        # self.__driver.find_element(By.XPATH, "//div[text()='Patient']").click()
+        super().click_on_element(self.__patient_locator)
 
     def click_on_new_search_menu(self):
-        self.__driver.find_element(By.XPATH, "//div[text()='New/Search']").click()
+        # self.__driver.find_element(By.XPATH, "//div[text()='New/Search']").click()
+        super().click_on_element(self.__new_search_locator)
