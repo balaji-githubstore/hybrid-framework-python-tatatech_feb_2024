22e4ea0e747d356c523147050a280f6dbb3c3c10
Balaji <balaji0017@gmail.com>
2024-02-09 08:34:23 +0000

    commented code

--- utils/zdemo1_pandas.py
+++ utils/zdemo1_pandas.py
@@ -1,33 +1,33 @@
-"""Will be deleted later - not part of the framework"""
-
-import pandas
-
-import config
-
-df=pandas.read_excel(io="../test_data/open_emr_data.xlsx",sheet_name="test_valid_login")
-print(df)
-# print(df.loc[1])
-# print(df.columns)
-# print(df.index)
-print(df.values.tolist())
-
-#alernate way
-ls=[]
-for i in df.index:
-    print(df.loc[i].tolist())
-    ls.append(df.loc[i].tolist())
-
-print(ls)
-
-df=pandas.read_csv(filepath_or_buffer="../test_data/test_valid_login.csv",delimiter=";")
-print(df)
-print(df.values.tolist())
-
-
-json_dic=pandas.read_json(path_or_buf="../test_data/config.json",typ="dictionary")
-print(json_dic)
-print(json_dic["browser"])
-print(json_dic["url"])
-
-print(config.project_path)
-print(config.test_data_path)
+# """Will be deleted later - not part of the framework"""
+#
+# import pandas
+#
+# import config
+#
+# df=pandas.read_excel(io="../test_data/open_emr_data.xlsx",sheet_name="test_valid_login")
+# print(df)
+# # print(df.loc[1])
+# # print(df.columns)
+# # print(df.index)
+# print(df.values.tolist())
+#
+# #alernate way
+# ls=[]
+# for i in df.index:
+#     print(df.loc[i].tolist())
+#     ls.append(df.loc[i].tolist())
+#
+# print(ls)
+#
+# df=pandas.read_csv(filepath_or_buffer="../test_data/test_valid_login.csv",delimiter=";")
+# print(df)
+# print(df.values.tolist())
+#
+#
+# json_dic=pandas.read_json(path_or_buf="../test_data/config.json",typ="dictionary")
+# print(json_dic)
+# print(json_dic["browser"])
+# print(json_dic["url"])
+#
+# print(config.project_path)
+# print(config.test_data_path)
